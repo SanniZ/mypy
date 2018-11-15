4a4645b415ba406dfaf75b3a2bd51614fc3c6778
yingbinx <yingbinx.zeng@intel.com>
2018-11-15 16:17:52 +0800

    upload Template files

---
+++ Template.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2018-11-15
+
+@author: Byng Zeng
+"""
+
+import re, sys, getopt
---
+++ Template_Class.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2018-11-15
+
+@author: Byng Zeng
+"""
+
+import re, sys, getopt
+
+class Template(object):
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+if __name__ == '__main__':
+    temp=Template()
+    temp.main()
