86b75b8ed40fda9ed1a0862d3e8e5d2e2ce99624
yingbinx <yingbinx.zeng@intel.com>
2018-11-16 15:05:46 +0800

    update iOSCalendarICS.py

--- iOS/iOSCalendarICS.py
+++ iOS/iOSCalendarICS.py
@@ -20,6 +20,8 @@ class CalendarEvent(object):
         self._description=None
         self._type=''
 
+
+
 class Calendar(object):
     RE_DTSTART=r'DTSTART;.+'
     RE_DTEND=r'DTEND;.+'
@@ -31,18 +33,24 @@ class Calendar(object):
     RE_DESCRIPTION=r'DESCRIPTION:.+'
     RE_CALNAME=r'X-WR-CALNAME:.+'
 
+    TYPE_FILE=r'FILE'
+    TYPE_DIR=r'DIR'
+    TYPE_DIR_COMBINE=r'DIR_COMBINE'
+
     def __init__(self):
         # init values.
         self._src=None
+        self._srcType=None
         self._tgt=None
+        self._tgtType=None
         self._fmt=None
         self._calName=None
         self._listEvents=[]
         self._listCnt=0
         self._event=CalendarEvent()
         #self._sortKey='DTSTART'
-        self._sortReverse=True
-        self._multiCombine=False
+        self._sortReverse=False
+        self._combineFiles=False
 
         # get all of re_compile.
         self._re_dtstart=re.compile(Calendar.RE_DTSTART)
@@ -54,8 +62,7 @@ class Calendar(object):
         self._re_sum=re.compile(Calendar.RE_SUMMARY)
         self._re_desc=re.compile(Calendar.RE_DESCRIPTION)
         self._re_calname=re.compile(Calendar.RE_CALNAME)
-        # get user options.
-        self.getUserOpt()
+
 
     def printHelp(self):
         print 'option: -s xxx -t xxx [-f xxx] [-r xxx] [-c]'
@@ -64,13 +71,14 @@ class Calendar(object):
         print '  -f txt/csv:'
         print '    format of output file'
         print '  -r True/False:'
-        print '    True is ascending sort, False is descending, default to True.'
+        print '    True is ascending sort, False is descending, default to False.'
         print '  -s xxx.ics or xxx/:'
         print '    xxx/ is a path, xxx.ics is a ics file'
         print '  -t xxx/ or xxx.xxx:'
         print '    xxx/ is is a path, xxx.xxx is the name of target'
-        # stop and exit.
+        # exit here
         exit()
+
 
     def getUserOpt(self):
         try:
@@ -82,7 +90,7 @@ class Calendar(object):
             if name == r'-h':
                 self.printHelp()
             elif name == r'-c':
-                self._multiCombine=True
+                self._combineFiles=True
             elif name == r'-f':
                 self._fmt=value
             elif name == r'-r':
@@ -94,38 +102,118 @@ class Calendar(object):
                 self._src=value
             elif name == r'-t':
                 self._tgt=value
-        # check args.
-        if self._src == None or self._tgt == None:
+
+
+    # check user args
+    def checkOptArgs(self):
+        # check src.
+        if self._src == None:
+            self._src=os.getcwd()
+            self._srcType=Calendar.TYPE_DIR
+        elif os.path.exists(self._src) == False:
+            print 'Error no found %s, pls use -s xxx to set source: xxx/xxx.ics or xxx/\n' % self._src
             self.printHelp()
+        elif os.path.isfile(self._src) == True:
+            # check external name
+            if os.path.splitext(self._src)[1] != r'.ics':
+                print 'Error, %s is not ics file!\n' % self._src
+                self.printHelp()
+            else:
+                self._srcType=Calendar.TYPE_FILE
+        elif os.path.isdir(self._src) == True:
+            self._srcType=Calendar.TYPE_DIR
+        else:
+            print 'Error, pls use -s xxx to set source: xxx/xxx.ics or xxx/'
+            self.printHelp()
+
+        # check tgt.
+        if self._fmt != None:
+            if self._tgt == None:
+                self._tgt=os.getcwd()
+                self._tgtType=Calendar.TYPE_DIR
+                #print 'No set target dir, default set to current path: %s.\n' % self._tgt
+            else:
+                extName=os.path.splitext(self._tgt)[1][1:]
+                if extName == r'':
+                    self._tgtType=Calendar.TYPE_DIR
+                    if self._tgt[len(self._tgt)-1] == r'/':
+                        self._tgt=self._tgt[:len(self._tgt)-1]
+                elif extName == self._fmt:
+                    if self._srcType == Calendar.TYPE_DIR:
+                        if self._combineFiles == False:
+                            opt=raw_input(r'Do you want to output all of data to %s(y/n)?' % os.path.basename(self._tgt))
+                            if opt.upper() == r'Y':
+                                self._combineFiles=True
+                                self._tgtType=Calendar.TYPE_FILE
+                            else:
+                                self._tgt=os.path.split(self._tgt)[0]
+                                self._tgtType=Calendar.TYPE_DIR
+                    else:
+                        self._tgtType=Calendar.TYPE_FILE
+                else:
+                    self._tgtType=Calendar.TYPE_DIR
+
 
     # get all of .ics file under folder.
     def getFilesOfPath(self):
-        self._src=os.path.dirname(self._src)
+        #self._src=os.path.dirname(self._src)
         return os.listdir(self._src)
 
+
     def getOutputFileName(self, fmt):
-        if self._tgt.find(r'.%s' % fmt) == -1:
-            name=r'%s/%s.%s' % (os.path.dirname(self._tgt), self._calName, fmt)
+        if self._tgtType == Calendar.TYPE_FILE:
+            # tgt is match fmt external name.
+            if self._fmt == os.path.splitext(self._tgt)[1][1:]:
+                name=self._tgt
+            else: # tgt is not match fmt external name, set name=%tgt.%fmt
+                name=r'%s.%s' % (self._tgt, self._fmt)
+        elif self._tgtType == Calendar.TYPE_DIR:
+            if self._combineFiles == True:
+	        name=r'%s/%s.%s' % (self._tgt, r'Combine', fmt)
+            else:
+	        name=r'%s/%s.%s' % (self._tgt, self._calName, fmt)
         else:
-            name=r'%s/%s' % (os.path.dirname(self._tgt), self._calName)
-        # remove old file.
-        if os.path.exists(name) == True:
-            os.remove(name)
+            print 'Error, unknown output format.'
+            self.printHelp()
         # return name of file.
         return name
 
-    # create table header
-    def createCSVTitle(self):
-        # create title if not set.
-        with open(self.getOutputFileName(r'csv'), r'a') as f:
-            if self._multiCombine == True:
-                f.write('开始日期,结束日期,开始时间,结束时间,地点,事件,描述,类型\n')
+
+    def readyForOutput(self, name):
+        path=os.path.split(name)[0]
+        if os.path.exists(path) == False:
+            os.mkdir(path)
+        if os.path.exists(name) == True:
+            os.remove(name)
+
+
+    def getOptType(self):
+        if self._srcType == Calendar.TYPE_FILE:
+            result=Calendar.TYPE_FILE
+        elif self._srcType == Calendar.TYPE_DIR:
+            if self._combineFiles == True:
+                result=Calendar.TYPE_DIR_COMBINE
             else:
-                f.write('开始日期,结束日期,开始时间,结束时间,地点,事件,描述\n')
+                result=Calendar.TYPE_DIR
+        #print 'operation type: %s' % result
+        # return result.
+        return result
+
 
     # save data to .csv file.
     def saveToCSV(self):
-        with open(self.getOutputFileName(r'csv'), r'a') as f:
+        # get output name
+        name=self.getOutputFileName(r'csv')
+        # ready for write
+        self.readyForOutput(name)
+        # write events.
+        with open(name, r'a') as f:
+            # create title if not set.
+            if self._combineFiles == True:
+                f.write('开始日期,结束日期,开始时间,结束时间,地点,事件,描述,类型\n')
+            else:
+                f.write('开始日期,结束日期,开始时间,结束时间,地点,事件,描述\n')
+            # start to write events.
             for event in self._listEvents:
                 # write start of date.
                 f.write('%s-%s-%s,' % (event._date_s[0:4], event._date_s[4:6], event._date_s[6:8]))
@@ -146,7 +234,7 @@ class Calendar(object):
                 # write summary.
                 f.write('%s,' % event._summary[len('SUMMARY:'):len(event._summary)-1])
                 # write type if it is combine option.
-                if self._multiCombine == True:
+                if self._combineFiles == True:
                     # write description.
                     if event._description == None:
                         f.write(',')
@@ -160,10 +248,16 @@ class Calendar(object):
                         f.write('\n')
                     else:
                         f.write('%s\n' % event._description[len('DESCRIPTION:'):len(event._description)-1])
+        print 'output: %s' % name
 
     # save data to .txt file.
     def saveToTXT(self):
-        with open(self.getOutputFileName(r'txt'), r'a') as f:
+        # get output name
+        name=self.getOutputFileName(r'txt')
+        # ready for write
+        self.readyForOutput(name)
+        # write events.
+        with open(name, r'a') as f:
             for event in self._listEvents:
                 # write new event.
                 f.write('----------------------------------------------\n')
@@ -184,6 +278,7 @@ class Calendar(object):
                     f.write('DESCRIPTION:\n')
                 else:
                     f.write('DESCRIPTION: %s\n' % event._description[len('DESCRIPTION:'):len(event._description) - 1])
+        print 'output: %s' % name
 
     def printICSContent(self):
         for event in self._listEvents:
@@ -205,8 +300,10 @@ class Calendar(object):
             else:
                 print 'DESCRIPTION: %s\n' % event._description[len('DESCRIPTION:'):len(event._description) - 1]
 
+
     def sortEvent(self, event):
         return event._date_s
+
 
     def formatOutput(self):
         if len(self._listEvents) == 0:
@@ -217,17 +314,22 @@ class Calendar(object):
         if self._fmt == r'txt':
             self.saveToTXT()
         elif self._fmt == r'csv':
-            self.createCSVTitle()
             self.saveToCSV()
-        else:
+        elif self._fmt == None:
             self.printICSContent()
+        else:
+            print 'Error, use -f txt/csv to set format output\n'
+            self.printHelp()
+
         # clear all of events.
         self._listEvents=[]
+
 
     def insertEvent(self):
         self._listEvents.append(self._event)
         # update for next event.
         self._event=CalendarEvent()
+
 
     def processICS(self, ics):
         if os.path.isfile(ics) != True:
@@ -282,32 +384,49 @@ class Calendar(object):
                 if summary != None:
                     self._event._summary=summary.group()
                     # add type of data
-                    if self._multiCombine == True:
+                    if self._combineFiles == True or self._fmt == None:
                         self._event._type=self._calName
                     # event is ready, insert to list now.
                     self.insertEvent()
 
+
+    def singleFileHandler(self, f):
+        self.processICS(f)
+        # output
+        self.formatOutput()
+
+
+    def dirFilesHandler(self):
+        fs=self.getFilesOfPath()
+        # loop for all of files.
+        for f in fs:
+            self.singleFileHandler(f)
+
+
+    def CombineDIRHandler(self):
+        fs=self.getFilesOfPath()
+        # loop for all of files.
+        for f in fs:
+            self.processICS(f)
+        # output
+        self.formatOutput()
+
+
     def main(self):
-        # no .ics, it is a path.
-        if self._src.find(r'.ics') != -1:
-            self.processICS(self._src)
-            self.formatOutput()
-        else:
-            fs=self.getFilesOfPath()
-            for f in fs:
-                if self._multiCombine == True:
-                    self.processICS(r'%s/%s' % (self._src, f))
-                else:
-                    self.processICS(r'%s/%s' % (self._src, f))
-                    self.formatOutput()
-            # get name of tgt
-            name=os.path.basename(self._tgt)
-            if name == '':
-                self._calName=r'Combine'
-            else:
-                self._calName=name
-            # output files.
-            self.formatOutput()
+        # get user options.
+        self.getUserOpt()
+        # check args.
+        self.checkOptArgs()
+        # get operation type.
+        optType=self.getOptType()
+        if optType == Calendar.TYPE_FILE:
+            self.singleFileHandler(self._src)
+        elif optType == Calendar.TYPE_DIR:
+            self.dirFilesHandler()
+        elif optType == Calendar.TYPE_DIR_COMBINE:
+            self.CombineDIRHandler()
+
+
 
 if __name__ == '__main__':
     cal=Calendar()
