65b738227208e797b98a8595802c28b07c8cf1c3
Byng Zeng <sanni230@126.com>
2018-12-13 06:36:50 +0800

    backup User-Agent

--- web/webcontent.py
+++ web/webcontent.py
@@ -24,7 +24,18 @@ URL_HEADER = {
     'Connection': 'keep-alive',
     #'Accept-Language': 'zh-CN,zh;q=0.8',
     #'Accept-Encoding': 'gzip, deflate, sdch, br',
+
     #'Cache-Control': 'max-age=0'
+    #'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36 LBBROWSER',
+    #'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+    #'User-Agent': 'Opera/9.25 (Windows NT 5.1; U; en)',
+    #'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+    #'User-Agent': 'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+    #'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+    #'User-Agent': 'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9',
+    #'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.7 (KHTML, like Gecko) Ubuntu/11.04 Chromium/16.0.912.77 Chrome/16.0.912.77 Safari/535.7',
+    #'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0',
+    #'Host': 'ptlogin2.qq.com',
 }
 
 
