04ba28b0e8224035a31f9cbf6c13dfce8f44dc17
yingbinx <yingbinx.zeng@intel.com>
2019-07-31 12:45:17 +0800

    update zephyr.py
    1. updated zephyr.py to 1.0.3.
    2. updated to filter cmd args.
    3. added search dirs to home and samples.

--- zephyr/zephyr.py
+++ zephyr/zephyr.py
@@ -14,34 +14,22 @@ import shutil
 from collections import OrderedDict
 
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 
 
 # default home of zephyr
-ZEPHYR_HOME = os.path.join(os.getenv('HOME'), "workspace/zephyr/master")
+ZEPHYR_HOME = os.getenv('ZEPHYR_SRC_HOME')
 
 
-# usage help
-#
-def usage_help():
-    USAGE = (
-        "---------------------------------------------",
-        "    Zephyr Tools  - %s" % VERSION,
-        "---------------------------------------------",
-        "",
-        "  usage: zephyr options",
-        "",
-        "options:",
-        "  -b | build  name,[path]:",
-        "    build application of zephyr.",
-        "  -r | run  name:",
-        "    run application of zephyr.",
-        "  -h | home  path",
-        "    set path of zephyr.",
-    )
-    for help in USAGE:
-        print(help)
-    sys.exit()
+def print_msg(msg, exit_=False):
+    t = type(msg)
+    if t == str:
+        print(msg)
+    elif t in [dict, tuple]:
+        for m in msg:
+            print(m)
+    if exit_:
+        sys.exit()
 
 
 # build name of application.
@@ -51,7 +39,7 @@ def build(args):
     def build_help():
         HELPS = (
             '',
-            '  usage: zephyr build name [src] [board] [home]',
+            'usage: zephyr build name [src] [board] [home]',
             '',
             'name or name=xxx:',
             '  name of application to build',
@@ -59,20 +47,13 @@ def build(args):
             '  path of src of application.',
             'board or board=xxx',
             '  board platform to build, default qemu_x86',
-            'home or home=xxx',
-            '  path of project',
         )
-        for help in HELPS:
-            print(help)
-        sys.exit()
+        print_msg(HELPS, 1)
 
     home = ZEPHYR_HOME
     name = src = None
     board = 'qemu_x86'
 
-    if not args:
-        print("Error, no found build args!")
-        return 0
     # update home.
     if 'home' in args:
         home = args['home']
@@ -87,13 +68,12 @@ def build(args):
                     src = cv[1]
                 elif cv[0] == 'board':
                     board = cv[1]
-                elif cv[0] == 'help':
+                elif cv[0] in ['-h', '--help']:
                     build_help()
                 else:
-                    print('Error, unknown %s to build' % cv[0])
-                    sys.exit()
+                    print_msg('Error, unknown %s to build' % cv[0], -1)
             else:
-                if val in ['-h', 'help', '--help']:
+                if val in ['-h', '--help']:
                     build_help()
                 if not name:
                     name = val
@@ -104,11 +84,16 @@ def build(args):
 
     if name:
         if not src:  # set default src to samples.
-            src = os.path.join('samples', name)
+            ds = [home, 'samples']
+            found_app = False
+            for d in ds:
+                src = os.path.join(d, name)
+                if os.path.exists(src):
+                    found_app = True
+                    break
         # check source code.
-        if not os.path.exists(os.path.join(home, src)):
-            print('No found source code of %s' % name)
-            return -1
+        if not found_app:
+            print_msg('No found source code of %s' % name, -1)
         # remove old out
         out = os.path.join('out', name)
         if os.path.exists(os.path.join(home, out)):
@@ -133,23 +118,18 @@ def run(args):
     def run_help():
         HELPS = (
             '',
-            '  usage: zephyr run name [path]',
+            'usage: zephyr run name [path]',
             '',
             'name or name=xxx:',
             '  name of application to build',
             'path or path=xxx',
             '  build path of application.',
         )
-        for help in HELPS:
-            print(help)
-        sys.exit()
+        print_msg(HELPS, 1)
 
     home = ZEPHYR_HOME
     name = path = None
 
-    if not args:
-        print("Error, no found build args!")
-        return 0
     # update home.
     if 'home' in args:
         home = args['home']
@@ -162,98 +142,129 @@ def run(args):
                     name = cv[1]
                 elif cv[0] == 'path':
                     path = cv[1]
-                elif cv[0] == 'help':
+                elif cv[0] in ['-h', '--help']:
                     run_help()
                 else:
-                    print('Error, unknown %s to run' % cv[0])
-                    sys.exit()
+                    print_msg('Error, unknown %s to run' % cv[0], -1)
             else:
-                if val in ['-h', 'help', '--help']:
+                if val in ['-h', '--help']:
                     run_help()
                 if not name:
                     name = val
+    if not name:
+        print_msg('Error, unknown name to run!', -1)
 
-    if (not path) and name:
+    if not path:
         path = os.path.join(home, 'out', name)
 
     if not os.path.exists(path):
-        print('No found application of %s' % name)
-        return -1
+        print_msg('No found application of %s' % name, -1)
     # run shell command to run target
     cmd = "cd {path} && make run".format(path=path)
     subprocess.call(cmd, shell=True, executable="/bin/bash")
     return 0
 
 
+# usage help
+#
+def usage_help(args=None):
+    USAGE = (
+        "---------------------------------------------",
+        "    Zephyr Tools  - %s" % VERSION,
+        "---------------------------------------------",
+        "",
+        "  usage: zephyr cmd [options]",
+        "",
+        "-b | build : name | src [board]",
+        "  build application of zephyr.",
+        "-r | run   : name | path",
+        "  run application of zephyr.",
+        "-H | home  : path",
+        "  set home of zephyr.",
+        "",
+        "--help for cmd help",
+    )
+    print_msg(USAGE, 1)
+
+
+# format:        name  : [min_args, [args], function]
+support_cmds = {
+    'build': [1, ['-b', 'build'], build],
+    'run': [1, ['-r', 'run'], run],
+    'home': [1, ['-H', 'home'], None],
+    'help': [0, ['-help', 'help'], usage_help],
+}
+
+
 # get cmds.
 #
 def get_cmds(args):
-    if not args:
-        usage_help()
-
-    support_cmds = {'build': ['-b', 'build'],
-                    'run': ['-r', 'run'],
-                    'home': ['-h', 'home'],
-                    }
-
     cmds = OrderedDict()
     cmd_pending = None
-    cmd_found = 0
+    cmd_found = None
     # get args of cmd.
     for cmd in args:
-        if not cmd_found:  # check new cmd.
-            for key, values in support_cmds.items():
-                if cmd in values:  # found supported cmd.
-                    cmd_pending = key
-                    cmds[cmd_pending] = []
-                    cmd_found = 1
-                    break
-            if cmd_found:  # get next cmd.
-                continue
+        for key, values in support_cmds.items():
+            if cmd in values[1]:  # found supported cmd.
+                cmd_pending = key
+                cmds[cmd_pending] = []
+                cmd_found = key
+                break
+        if cmd_found:  # get next cmd.
+            cmd_found = None
+            continue
         # get args for cmd.
         if cmd_pending:
             cmds[cmd_pending].append(cmd)
-            cmd_found = 0
+    # check cmd args.
+    for cmd in cmds:
+        if len(cmds[cmd]) < support_cmds[cmd][0]:
+            print_msg('Error, no args for %s, --help for help' % cmd, -1)
     return cmds
 
 
 # run cmds
 #
 def run_cmds(cmds):
-    support_cmds = {'build': build,
-                    'run': run,
-                    }
-
-    if not cmds:
-        usage_help()
-        return -1
-
-    home = ZEPHYR_HOME
-    # check home
-    if 'home' in cmds:
-        if len(cmds['home']):
-            home = cmds['home'][0]
+    rc = 0
+    if cmds:
+        # set home
+        if 'home' in cmds:
+            if len(cmds['home']):
+                home = cmds['home'][0]
+            else:
+                print_msg('Error, no args for home!', -1)
+            del cmds['home']  # remove home from cmds.
         else:
-            print('Error, no args for home!')
-            return -1
-        del cmds['home']  # remove home from cmds.
-    # run cmds.
-    for key, value in cmds.items():
-        if key in support_cmds:
-            args = {}
-            args['home'] = home
-            args['args'] = value
-            support_cmds[key](args)
-    return 0
+            if ZEPHYR_HOME:
+                home = ZEPHYR_HOME
+            else:
+                print_msg('Error, no get home value, '
+                          'or set default home "export ZEPHYR_SRC_HOME=xxx"',
+                          -1)
+        # run cmds.
+        for key, value in cmds.items():
+            if key in support_cmds:
+                args = {}
+                args['home'] = home
+                args['args'] = value
+                rc = support_cmds[key][2](args)
+    return rc
 
 
 # main
 #
 def main(args=None):
+    if not args:
+        usage_help()
+    # get cmd and args.
     cmds = get_cmds(args)
-    run_cmds(cmds)
-    return 0
+    if cmds:
+        run_cmds(cmds)
+    else:
+        usage_help()
 
 
+# entrance.
 if __name__ == '__main__':
     main(sys.argv[1:])
