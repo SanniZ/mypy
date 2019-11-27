06ababfde4dc31b8ad06b80e7537154d4487756c
Byng Zeng <sanni230@126.com>
2019-11-27 15:51:03 +0800

    update mdcrypto.py
    updated mdcrypto for call c share library.
    it will call mdkey.so to get key for encrypto/decrypto.

--- tools/markdown/mdcrypto.py
+++ tools/markdown/mdcrypto.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 
-VERSION='1.1.0'
+AUTHOR  = 'Byng Zeng'
+VERSION = '1.2.0'
 
 import os
 import sys
 import subprocess
+import getpass
+from ctypes import *
 
 from pybase.pyinput import get_input_args
 from pybase.pysys import print_help, print_exit
 from pybase.pyfile import remove_type_file
 from crypto.cryptofile import CryptoFile
 
-import getpass
-
-KEY = "Byng-00-markdown"
 
 def markdown_help():
     help_menus = (
         "=====================================",
         "    markdown crypto - %s" % VERSION,
         "=====================================",
-        " options: [-k xxx] [-s xxx] [-o xxx] -d/-e"
+        " options: [-k xxx] [-s xxx] [-o xxx] -d/-e",
         " -h: print help",
         " -k: 16 Bytes key password",
         " -s: path of source",
@@ -29,6 +29,20 @@ def markdown_help():
         " -e: encrypto markdown",
     )
     print_help(help_menus, True)
+
+
+def markdown_get_key_from_so():
+    mdkey_so = os.getenv("MDKEY_SO")
+    if not os.path.exists(mdkey_so):
+        print('Error, no found mdkey.so, set export MDKEY_SO to .bashrc')
+        return None
+    lib = cdll.LoadLibrary(mdkey_so)
+    func_get_md_key = lib.get_md_key
+    func_get_md_key.argtype = (c_char_p)
+    func_get_md_key.restype = (c_char_p)
+    pwd = create_string_buffer(16)
+    pwd.raw = str(getpass.getpass("Input your pwd:")).encode()
+    return func_get_md_key(pwd)
 
 
 def markdown_encrypto(key, src, dst):
@@ -42,32 +56,34 @@ def markdown_decrypto(key, src, dst):
 
 
 def markdown(args=None):
-    key = KEY
+    key = None
     src = os.getcwd()
     dst = src
     opts = []
     if not args:
         args = get_input_args('dek:s:o:h')
-    # get key.
-    if '-k' not in args:
-        k = getpass.getpass('input your key:')
-        if k:
-            args['-k'] = k
     # set vars.
     for k, v in args.items():
         if k == '-k':
-            key = v
+            key = v[0]
         elif k == '-s':
-            src = os.path.abspath(v)
+            src = os.path.abspath(v[0])
             dst = src
         elif k == '-o':
-            dst = os.path.abspath(v)
+            dst = os.path.abspath(v[0])
         elif k == '-d':
             opts.append(k)
         elif k == '-e':
             opts.append(k)
         else:
             markdown_help()
+    # get key.
+    if not key:
+        key = markdown_get_key_from_so()
+        if not key:
+            # key = getpass.getpass('input your key:')
+            print("Error, found invalid pwd!")
+            return None
     # run opts.
     for opt in opts:
         if opt == '-d':
@@ -77,6 +93,4 @@ def markdown(args=None):
 
 
 if __name__ == "__main__":
-    # if getpass.getpass('input your key:') != 'xxx':
-    #     print_exit("error, invalid key!", True)
     markdown()
